371df43ee17cef97f0e5c89796f941c774b27ded
jx0905 <74499182+jx0905@users.noreply.github.com>
2022-03-27 23:50:31 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "employee.c4efxykm2sat.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "bait3273"
+customdb = "employee"
+custombucket = "linjingxuan-employee"
+customregion = "us-east-1"
